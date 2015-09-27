72f327d7122212e407b4a316f9db6fecebcc398b
Bill Brock <william.e.brock@gmail.com>
2015-09-27 09:29:33 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+var bot = require('./bot');
+
+// body parser middleware
+app.use(bodyParser.urlencoded( { extended: true}));
+
+// routes
+app.get('/', function(req, res) { res.status(200).send('Hello World');});
+app.post('/hello', bot);
+
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
+
+app.listen(port, function() {
+  console.log('Slack bot is listening on port ' + port);
+});
